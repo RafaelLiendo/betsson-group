17d4f5570aed66c13559957a356df247cda202d6
Rafael Liendo <rafael.n.liendo@gmail.com>
2018-08-25 22:50:20 -0300

    Rename Project

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace betsson_group.Controllers
+namespace BetssonGroup.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
