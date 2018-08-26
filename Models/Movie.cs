1e715fab227be900a39da4b6777b680a429ec8e7
Rafael Liendo <rafael.n.liendo@gmail.com>
2018-08-26 16:55:28 -0300

    add backend models

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetssonGroup.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Genre> Genres { get; set; } = new List<Genre>();
+        public float Rate { get; set; }
+        public string Length { get; set; }
+        public string Img { get; set; }
+    }
+}
