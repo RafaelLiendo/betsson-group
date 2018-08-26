11c96fec015762a28b1fbe03431cc078e8415022
Rafael Liendo <rafael.n.liendo@gmail.com>
2018-08-26 15:26:25 -0300

    fix typo

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '', pathMatch: 'full', redirectTo: 'movies' },
   // { path: 'dashboard', component: DashboardComponent },
   {
     path: '**',
     pathMatch: 'full',
-    loadChildren: './not-found/not-found.moduleh#NotFoundModule'
+    loadChildren: './not-found/not-found.module#NotFoundModule'
   }
 ];
 
