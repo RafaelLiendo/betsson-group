dda4c7b6634b92f53984629cde40efe2080234ea
Rafael Liendo <rafael.n.liendo@gmail.com>
2018-08-26 23:15:51 -0300

    add ngrx/effects

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent, ShellComponent],
@@ -26,7 +28,8 @@ import { reducers, metaReducers } from './reducers';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    StoreModule.forRoot(reducers, { metaReducers })
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
